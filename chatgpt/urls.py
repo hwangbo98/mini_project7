79be9134116c6f6eb8e095690ba4391e1a9ea6b0
nunssuby <nunssuby@gmail.com>
2024-05-22 19:13:25 +0900

    ajax처리 중 중간저장

--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('chat', views.chat, name='chat'),
     path('chatbot', views.chatbot, name='chatbot'),
+    path('chatbot_chat', views.chatbot_chat, name='chatbot_chat'),
 ]
