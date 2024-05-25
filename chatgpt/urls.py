afa7df18b8ddac3536d7eb44a7af5b4eb5e93d6f
nunssuby <nunssuby@gmail.com>
2024-05-26 08:39:09 +0900

    메모리 챗봇 완성

--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -6,6 +6,14 @@ app_name = 'chatgpt'
 urlpatterns = [
     path('', views.index, name='index'),
     path('chat', views.chat, name='chat'),
-    path('chatbot', views.chatbot_form, name='chatbot'),
-    path('chatbot_chat', views.chatbot_chat, name='chatbot_chat'),
+
+    path('simple_chatbot', views.simple_chatbot, name='simple_chatbot'),
+
+    # path('chatbot_memory', views.chatbot_memory_form, name='chatbot_memory'),
+    # path('chatbot_memory_chat', views.chatbot_memory_chat, name='chatbot_memory_chat'),
+
+    path('memory_chatbot', views.memory_chatbot, name='memory_chatbot'),
+
+
+    
 ]
